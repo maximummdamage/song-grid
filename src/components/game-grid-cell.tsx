4e94b73fe82671563fd8d0f8899da47197d24f15
Maxwell Dupree <maxdupree17@gmail.com>
2024-11-21 15:23:26 -0600

    minimal effort

---
+++ src/components/game-grid-cell.tsx
@@ -0,0 +1,11 @@
+import { Card, Typography } from '@mui/material';
+
+const GameGridCell = () => {
+    return (
+        <Card className='game-grid-cell'>
+            <Typography>guess here</Typography>
+        </Card>
+    );
+};
+
+export default GameGridCell;
